bfabdaa149f201c00b7176a7d913183a41b20559
Garima Goyal <goyalanita254@gmail.com>
2026-07-15 14:55:56 +0530

    Fifth commit

--- revArray.c
+++ revArray.c
@@ -1,41 +1,14 @@
-// Problem: Given an array of n integers, reverse the array in-place using two-pointer approach.
-// Input:
-// - First line: integer n
-// - Second line: n space-separated integers
-// Output:
-// - Print the reversed array, space-separated
-// Example:
-// Input:
-// 5
-// 1 2 3 4 5
-// Output:
-// 5 4 3 2 1
-// Explanation: Swap pairs from both ends: (1,5), (2,4), middle 3 stays
+int removeElement(int* nums, int numsSize, int val) {
 
-#include <stdio.h>
-int main(){
-    int n;
-    scanf("%d", &n);
-    int arr[n];
-    for(int i=0; i<n; i++){
-        scanf("%d", &arr[i]);
+    int k = 0;
+
+    for(int i = 0; i < numsSize; i++) {
+
+        if(nums[i] != val) {
+            nums[k] = nums[i];
+            k++;
+        }
     }
 
-    int left = 0;
-    int right = n - 1;
-    int temp;
-
-    while (left < right) {
-        temp = arr[left];
-        arr[left] = arr[right];
-        arr[right] = temp;
-
-        left++;
-        right--;
-    }
-    printf("Reversed Array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    return 0;
+    return k;
 }
